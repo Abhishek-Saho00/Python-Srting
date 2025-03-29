2169e95639e35610045eddc882d7de5087b55181
Abhisek-Saho00 <9810218964abhi@gmail.com>
2025-03-29 07:51:47 +0545

    test

---
+++ Ashish.js
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Darkworld</title>
+</head>
+<body>
+<script type = "text/javascript">
+
+document.write("Hello World");
+</scrpit>
+</body>
+</html>
--- __pycache__/Pyautogui.py
+++ __pycache__/Pyautogui.py
@@ -1,6 +1,6 @@
 import pyautogui as pg
 import time
-
+import pyautogui
 time.sleep(5)
 for i in range(100):
 	pg.write("Hi everyone how are you i am a coderHi everyone how are you i .🥰🥰🥰🥰🥰" )
